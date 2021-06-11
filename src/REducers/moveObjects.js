d0927d0bd7c09579d918c57ab05f5d18b907743c
Hiram-Sant <83189793+Hiram-Sant@users.noreply.github.com>
2021-06-10 22:39:00 -0500

    canyon ya funcionando

--- src/REducers/moveObjects.js
+++ src/REducers/moveObjects.js
@@ -1,6 +1,8 @@
 import { calculateAngle } from '../Utileria/Formulas';
 //ACCION DEL CANYON
 //PROBAR PARA LOS PROYECTILES 
+//elimina xy y las mousePosition transmite para que funcionen calculateAngle
+//en un nuevo ángulo. genera nuevo objetocon un nuevo valor de ángulo.
 function moveObjects(state, action) {
   if (!action.mousePosition) return state;
   const { x, y } = action.mousePosition;
